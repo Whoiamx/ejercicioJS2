3548372f09ef53346a3f8f323d8d60166446ac48
Whoiamx <gaastontimchuk@gmail.com>
2023-12-02 20:49:41 -0300

    Se inicia el proyecto del ejercicio 2 JS

---
+++ index.js
@@ -0,0 +1,62 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+const pizzasIdImpares = pizzas.filter((pizzas) => {
+  return pizzas.id % 2 == !0;
+});
+
+console.log(pizzasIdImpares);
+
+const filterPizzas = pizzas.filter((pizzas) => pizzas.precio < 600);
+console.log(filterPizzas);
+
+pizzas.forEach((pizzas) => {
+  console.log(`La pizza ${pizzas.nombre} sale $ ${pizzas.precio}`);
+});
